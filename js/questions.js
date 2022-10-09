7bfc904a1515fe8cfa36b5b00dd60e7a1ee517bf
Jasser000 <zaagjasser@gmail.com>
2022-10-09 01:16:03 +0100

    first commit

---
+++ js/questions.js
@@ -0,0 +1,73 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is a correct syntax to output \"Hello World\"in C?",
+    answer: "printf(\"Hello World\");",
+    options: [
+      "System.out.printline(\"Hello World\");",
+      "printf(\"Hello World\");",
+      "cout<<\"Hello World\";",
+      "Console.WriteLine(\"Hello World\");"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How do you insert COMMENTS in C code?",
+    answer: "//This is a comment",
+    options: [
+      "//This is a comment",
+      "--This is a comment",
+      "#This is a comment",
+      "/*This is a comment"
+    ]
+  },
+    {
+    numb: 3,
+    question: "C Language is ",
+    answer: "Low level Langauge",
+    options: [
+      "High level Language",
+      "Middle level Language",
+      "Low level Langauge",
+      "None above"
+
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following operator can be used to access value at address stored in a pointer variable?",
+    answer: "*",
+    options: [
+      "*",
+      "#",
+      "&&",
+      "@"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How can you create a variable with the numeric value 5?",
+    answer: "int num=5;",
+    options: [
+      "num=5;",
+      "int num=5;",
+      "num=5 int;",
+      "val num=5;"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
